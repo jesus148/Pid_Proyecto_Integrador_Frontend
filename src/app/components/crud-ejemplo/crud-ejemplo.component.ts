9967d22a0d92bbd34b8f1825038c7309c7236d03
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2024-06-17 22:29:15 -0500

    crud finalizado

--- src/app/components/crud-ejemplo/crud-ejemplo.component.ts
+++ src/app/components/crud-ejemplo/crud-ejemplo.component.ts
@@ -10,6 +10,9 @@ import { Ejemplo } from '../../models/ejemplo.model';
 import { EjemploService } from '../../services/ejemplo.service';
 import { TokenService } from '../../security/token.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { CrudEjemploAgregarComponent } from '../crud-ejemplo-agregar/crud-ejemplo-agregar.component';
+import { CrudEjemploActualizarComponent } from '../crud-ejemplo-actualizar/crud-ejemplo-actualizar.component';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -41,6 +44,31 @@ export class CrudEjemploComponent {
         this.objUsuario.idUsuario = tokenService.getUserId();
     }
 
+  openDialogRegistrar() {
+      console.log(">>> openDialogRegistrar [ini]");
+      const dialogRef = this.dialogService.open(CrudEjemploAgregarComponent);
+      dialogRef.afterClosed().subscribe(result => {
+            console.log('Dialog closed with result:', result);
+            if (result != null && result === 1) {
+              this.refreshTable();
+            }
+      });
+      console.log(">>> openDialogRegistrar [fin]");
+    }
+  
+    openDialogActualizar(obj: Ejemplo) {
+      console.log(">>> openDialogActualizar [ini]");
+      console.log("obj: ", obj);
+      const dialogRef = this.dialogService.open(CrudEjemploActualizarComponent, {data: obj} );
+      dialogRef.afterClosed().subscribe(result => {
+          console.log('Dialog closed with result:', result);
+          if (result != null && (result === 1 || result === 2)) {
+                this.refreshTable();
+          }
+      });
+      console.log(">>> openDialogActualizar [fin]");
+    }
+
     refreshTable(){
         console.log(">>> refreshTable [ini]");
         var msgFiltro = this.filtro == "" ? "todos":  this.filtro;
@@ -54,6 +82,38 @@ export class CrudEjemploComponent {
         console.log(">>> refreshTable [fin]");
     }
 
+  updateEstado(obj:Ejemplo) {
+      console.log(">>> updateEstado [ini]");
+      console.log("obj: ", obj);
+      obj.estado = obj.estado == 1 ? 0 : 1;
+      this.ejemploService.actualizarCrud(obj).subscribe(
+          x => {
+              this.refreshTable();
+          }
+      );
+       console.log(">>> updateEstado [fin]");
+  }
   
+  delete(obj: Ejemplo) {
+      Swal.fire({
+        title: '¿Desea eliminar?',
+        text: "Los cambios no se van a revertir",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, elimina',
+        cancelButtonText: 'No, cancelar'
+        }).then((result) => {
+              if (result.isConfirmed) {
+                  this.ejemploService.eliminarCrud(obj.idEjemplo || 0).subscribe(
+                        x => {
+                              this.refreshTable();
+                              Swal.fire('Mensaje', x.mensaje, 'info');
+                        }
+                  );
+              }
+        })   
+  }
 
 }
--- src/app/services/ejemplo.service.ts
+++ src/app/services/ejemplo.service.ts
@@ -19,10 +19,16 @@ export class EjemploService {
     return this.http.post(baseUrlPrueba, data);
   }
 
+  //Validaciones para PC1 y PC2
   validaDescripcionRegistra(descripcion: string): Observable<any>{
     console.log('>>> Service >> validaDescripcionRegistra [inicio]' + descripcion);
     return this.http.get<any>(baseUrlPrueba+'/validaDescripcionRegistra?descripcion='+descripcion);
   }
+  validaDescripcionActualiza(descripcion: string, id:number): Observable<any>{
+    console.log('>>> Service >> validaDescripcionActualiza [inicio]' + descripcion);
+    return this.http.get<any>(baseUrlCrudPrueba+'/validaDescripcionActualiza?descripcion='+descripcion + "&idEjemplo="+id);
+  }
+
 
   //PC2: CRUD de Ejemplo
   registrarCrud(data:Ejemplo):Observable<any>{
